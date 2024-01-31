c6fd82f825084bfa8aeeb0c45bd1b88218fd20dc
KseniaLass <ksenialass@gmail.com>
2024-01-31 11:42:06 +0300

    initial vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>PPDEV</h1>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <form action="#">
+
+    </form>
+  </main>
+</template>
